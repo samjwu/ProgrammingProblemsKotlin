b7c9317d58709fdea9bf1b8ad406435a61a20ab5
samjwu <sjwu@ualberta.ca>
2023-07-12 18:21:54 -0600

    2652

---
+++ 2652/Solution.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun sumOfMultiples(n: Int): Int {
+        var ans: Int = 0
+        
+        for (i in 1..n) {
+            if (i%3 == 0 || i%5 == 0 || i%7 == 0) {
+                ans += i
+            }
+        }
+        
+        return ans
+    }
+}
